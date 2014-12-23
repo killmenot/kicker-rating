ede4de9410e64d7c90f3665394654b91659df8ca
Ivan Sklyarov <sklyarov.161@gmail.com>
2014-12-23 14:50:16 +0300

    issue #2 update routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
+var models  = require('../models');
 
-module.exports = function () {
+module.exports = function (access) {
   router.get('/', function (req, res) {
     res.render('index', { title: 'Home' });
   });
@@ -10,6 +11,29 @@ module.exports = function () {
   router.get('/login', function (req, res) {
     res.render('login', { title: 'Login' });
   });
+
+
+  router.post('/api/season', access.if_logged_in_as_admin(), function (req, res) {
+      if (req.body.name.length === 0 && req.body.end.length === 0) {
+        res.status(400);
+        res.json({error:'name or date ended should be filled'});
+      }
+      if (req.body.end.length === 0) { req.body.end=null; }
+      models.Season.build({
+        date_started:req.body.start,
+        date_ended:req.body.end,
+        note:req.body.note,
+        name:req.body.name
+      }).save()
+      .then(function(data){
+        res.json(data.dataValues);
+      })
+      .catch(function(err){
+        res.status(400);
+        res.json({error:err.message});
+      });
+  });
+
 
   router.post('/login', passport.authenticate('local', { successReturnToOrRedirect: '/admin', failureRedirect: '/login' }));
 
