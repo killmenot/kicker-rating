0b5ce6bb006834ba878e9b906a34e88869a13edf
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-06 16:32:44 +0300

    Added links to bootstrap and jquery to master layout.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Kicker-Rating - @yield('title')</title>
+        <link href="{{ URL::asset('vendor/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('vendor/bootstrap/dist/css/bootstrap-theme.min.css') }}" rel="stylesheet">
+        <script src="{{ URL::asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+        <script src="{{ URL::asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     </head>
     <body>
         <div class="container">
