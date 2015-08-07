07e8f3002a739f2da9c3e2da0fa594c5dac8f5f0
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-07 16:28:32 +0300

    Added using a database cleanup functionality for tests.

--- tests/AuthorizationTest.php
+++ tests/AuthorizationTest.php
@@ -4,26 +4,35 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class AuthorizationTests extends TestCase
+class AuthorizationTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
     /**
      * User registration
      */
     public function testRegisterUser()
     {
+        $this->visit('/auth/logout');
         $this->visit('/auth/register')
             ->type('Test', 'name')
             ->type('test@test.com', 'email')
             ->type('123456', 'password')
-            ->type('123456', 'confirm_password')
+            ->type('123456', 'password_confirmation')
             ->press('Register')
-            ->seePageIs('/dashboard')
-            ->press('Dropdown')
-            ->press('Logout');
+            ->seePageIs('/dashboard');
+        $this->seeInDatabase('users', ['email' => 'test@test.com']);
     }
 
     public function testLoginUser()
     {
+        $this->visit('/auth/logout');
+        $this->seed();
         $this->visit('/auth/login')
             ->type('test@test.com', 'email')
             ->type('123456', 'password')
