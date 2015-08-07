9f1af661c21cf910d7cfaa084e80689388387094
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-07 15:12:41 +0300

    Remade errors form for layout to be as should laravel docs specify

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -57,7 +57,7 @@
                             <li><a href="#">Another action</a></li>
                             <li><a href="#">Something else here</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
+                            <li><a href="{{ URL::action('Auth\AuthController@getLogout') }}">Logout</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -65,8 +65,8 @@
         </div><!-- /.container-fluid -->
     </nav>
         <div class="container">
-           @if(isset($errors))
-               @foreach($errors as $error)
+           @if(count($errors->all()))
+               @foreach($errors->all() as $error)
                     <p class="bg-danger">{!! $error !!}</p>
                 @endforeach
             @endif
