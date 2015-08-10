81ba71c3f00a0497511f10921414d3e13fc2219a
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-10 14:53:12 +0300

    If user logged in, his name will appear at nav-bar with user menu.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,17 +50,23 @@
                     <button type="submit" class="btn btn-default">Submit</button>
                 </form>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="{{ URL::action('Auth\AuthController@getLogout') }}">Logout</a></li>
-                        </ul>
-                    </li>
+
+                    @if(Auth::check())
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="{{ URL::action('Auth\AuthController@getLogout') }}">Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
