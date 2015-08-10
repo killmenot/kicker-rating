3cfb8049f5e92155760509aaa4a79e12d69b6bb0
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-10 19:37:18 +0300

    Migrations for models added.

--- database/migrations/2015_08_10_131125_create_locations_table.php
+++ database/migrations/2015_08_10_131125_create_locations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLocationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')
-                ->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->integer('user_id')
+                ->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
         });
     }
 
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('locations');
     }
 }
