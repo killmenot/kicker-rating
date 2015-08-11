b658dedfc6640d7ed9d924b5a52a5511471be6e0
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-11 16:06:04 +0300

    Changed layout to show page user located first, then web site name.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Kicker-Rating - @yield('title')</title>
+        <title>@yield('title') - Kicker-Rating</title>
         <link href="{{ URL::asset('/css/main.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('vendor/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('vendor/bootstrap/dist/css/bootstrap-theme.min.css') }}" rel="stylesheet">
