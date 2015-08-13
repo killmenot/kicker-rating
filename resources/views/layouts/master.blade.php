b21778397f52936675b9fae83a6ef19addcb0d73
Dmitry Kurilenko <dmitry@intspirit.com>
2015-08-13 15:43:41 +0300

    Changed navbar style.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,8 +15,8 @@
         <script src="{{ URL::asset('js/layouts/master/account_menu.js') }}"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default">
-        <div class="container-fluid">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -25,21 +25,22 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Kicker-Rating</a>
+                <a class="navbar-brand" href="#">Rating</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="navbar">
                 <ul class="nav navbar-nav">
-                    
+
                     <li class="active">
                         <a href="{{ URL::action('Dashboard\LocationsController@getLocations') }}">
                             Locations
                             <span class="sr-only">(current)</span>
                         </a>
                     </li>
-                    
+
                     <li><a href="#">Link</a></li>
+
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                         <ul class="dropdown-menu">
@@ -52,13 +53,16 @@
                             <li><a href="#">One more separated link</a></li>
                         </ul>
                     </li>
+
                 </ul>
+
                 <form class="navbar-form navbar-left" role="search">
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Search">
                     </div>
                     <button type="submit" class="btn btn-default">Submit</button>
                 </form>
+
                 <ul class="nav navbar-nav navbar-right">
 
                     @if(Auth::check())
@@ -80,9 +84,13 @@
     </nav>
         <div class="container-fluid">
            @if(count($errors->all()))
+
                @foreach($errors->all() as $error)
+
                     <div class="alert alert-danger">{!! $error !!}</div>
+
                 @endforeach
+
             @endif
 
             @yield('content')
